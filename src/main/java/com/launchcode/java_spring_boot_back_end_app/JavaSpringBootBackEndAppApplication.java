9b78d84939eb84a21cf0324b07a9f950baa4675a
katemzr <katemzr@gmail.com>
2026-09-08 15:11:40 -0500

    Set up configurations and environmental variables. Connected to database successfully.

---
+++ src/main/java/com/launchcode/java_spring_boot_back_end_app/JavaSpringBootBackEndAppApplication.java
@@ -0,0 +1,13 @@
+package com.launchcode.java_spring_boot_back_end_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringBootBackEndAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringBootBackEndAppApplication.class, args);
+	}
+
+}
