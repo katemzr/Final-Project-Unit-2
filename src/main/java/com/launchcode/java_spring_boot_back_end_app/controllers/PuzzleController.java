45bc3ee6b31e6144aa3f56163baaa35441662a8b
katemzr <katemzr@gmail.com>
2026-09-11 14:30:25 -0500

    create @PutMapping in PuzzleController.java for updating an existing puzzle.

--- src/main/java/com/launchcode/java_spring_boot_back_end_app/controllers/PuzzleController.java
+++ src/main/java/com/launchcode/java_spring_boot_back_end_app/controllers/PuzzleController.java
@@ -28,5 +28,26 @@ public class PuzzleController {
         return puzzleRepository.save(puzzle);
     }
 
+    @PutMapping("/{id}")
+    public Puzzle updatePuzzle(@PathVariable int id, @RequestBody Puzzle updatedPuzzle) {
+        return puzzleRepository.findById(id).map(puzzle -> {
+            puzzle.setTitle(updatedPuzzle.getTitle());
+            puzzle.setBrand(updatedPuzzle.getBrand());
+            puzzle.setArtist(updatedPuzzle.getArtist());
+            puzzle.setPieceCount(updatedPuzzle.getPieceCount());
+            puzzle.setHeight(updatedPuzzle.getHeight());
+            puzzle.setWidth(updatedPuzzle.getWidth());
+            puzzle.setLocation(updatedPuzzle.getLocation());
+            puzzle.setPurchaseDate(updatedPuzzle.getPurchaseDate());
+            puzzle.setRetailer(updatedPuzzle.getRetailer());
+            puzzle.setStartDate(updatedPuzzle.getStartDate());
+            puzzle.setNotes(updatedPuzzle.getNotes());
+            puzzle.setProgressPercent(updatedPuzzle.getProgressPercent());
+            puzzle.setCompletionTime(updatedPuzzle.getCompletionTime());
+
+            return puzzleRepository.save(puzzle);
+        }).orElse(null);
+    }
+
 
 }
