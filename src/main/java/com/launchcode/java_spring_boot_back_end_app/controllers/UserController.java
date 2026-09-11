97194dc6d3b1ac6d93b82dab968ec91616bc8076
katemzr <katemzr@gmail.com>
2026-09-11 16:28:17 -0500

    completed adding CRUD ops to UserController.java

--- src/main/java/com/launchcode/java_spring_boot_back_end_app/controllers/UserController.java
+++ src/main/java/com/launchcode/java_spring_boot_back_end_app/controllers/UserController.java
@@ -1,5 +1,6 @@
 package com.launchcode.java_spring_boot_back_end_app.controllers;
 
+import com.launchcode.java_spring_boot_back_end_app.models.Puzzle;
 import com.launchcode.java_spring_boot_back_end_app.models.User;
 import com.launchcode.java_spring_boot_back_end_app.repositories.UserRepository;
 import org.springframework.web.bind.annotation.*;
@@ -16,5 +17,29 @@ public class UserController {
     @GetMapping
     public List<User>getAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable int id) {
+        return userRepository.findById(id).orElse(null);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable int id, @RequestBody User updatedUser) {
+        return userRepository.findById(id).map(user -> {
+            user.setEmail(updatedUser.getEmail());
+            user.setPassword(updatedUser.getPassword());
+            return userRepository.save(user);
+        }).orElse(null);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable int id) {
+        userRepository.deleteById(id);
     }
 }
--- src/main/java/com/launchcode/java_spring_boot_back_end_app/repositories/UserRepository.java
+++ src/main/java/com/launchcode/java_spring_boot_back_end_app/repositories/UserRepository.java
@@ -3,5 +3,5 @@ package com.launchcode.java_spring_boot_back_end_app.repositories;
 import com.launchcode.java_spring_boot_back_end_app.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User,Long> {
+public interface UserRepository extends JpaRepository<User,Integer> {
 }
