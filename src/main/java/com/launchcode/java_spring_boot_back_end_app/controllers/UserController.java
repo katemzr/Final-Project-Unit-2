fa311e99844aaaba844c3c5ade529ec6b70d1dca
katemzr <katemzr@gmail.com>
2026-09-11 16:10:38 -0500

    added imports to UserRepository.java. Also added extends of JPARepository

--- src/main/java/com/launchcode/java_spring_boot_back_end_app/controllers/UserController.java
+++ src/main/java/com/launchcode/java_spring_boot_back_end_app/controllers/UserController.java
@@ -5,6 +5,16 @@ import com.launchcode.java_spring_boot_back_end_app.repositories.UserRepository;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
 
+@RestController
+@RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User>getAllUsers() {
+        return userRepository.findAll();
+    }
 }
--- src/main/java/com/launchcode/java_spring_boot_back_end_app/repositories/UserRepository.java
+++ src/main/java/com/launchcode/java_spring_boot_back_end_app/repositories/UserRepository.java
@@ -1,4 +1,7 @@
 package com.launchcode.java_spring_boot_back_end_app.repositories;
 
-public interface UserRepository {
+import com.launchcode.java_spring_boot_back_end_app.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
 }
