1aabec3829412b3b41109d305a3e8b79964681e2
katemzr <katemzr@gmail.com>
2026-09-11 13:19:34 -0500

    added imports for PuzzleController.java. Added @GetMapping for all puzzles.

--- src/main/java/com/launchcode/java_spring_boot_back_end_app/controllers/PuzzleController.java
+++ src/main/java/com/launchcode/java_spring_boot_back_end_app/controllers/PuzzleController.java
@@ -1,6 +1,20 @@
 package com.launchcode.java_spring_boot_back_end_app.controllers;
 
+import com.launchcode.java_spring_boot_back_end_app.models.Puzzle;
+import com.launchcode.java_spring_boot_back_end_app.repositories.PuzzleRepository;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.List;
 
+@RestController
+@RequestMapping("/puzzles")
 public class PuzzleController {
+    @Autowired
+    private PuzzleRepository puzzleRepository;
+
+    @GetMapping
+    public List<Puzzle>getAllPuzzles() {
+        return puzzleRepository.findAll();
+    }
 }
