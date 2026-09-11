2e318b90db0edf8fddc9004f34f72a5e3327ba5e
katemzr <katemzr@gmail.com>
2026-09-11 16:03:41 -0500

    created UserRepository.java file. Created UserController.java file and added imports.

---
+++ src/main/java/com/launchcode/java_spring_boot_back_end_app/controllers/UserController.java
@@ -0,0 +1,10 @@
+package com.launchcode.java_spring_boot_back_end_app.controllers;
+
+import com.launchcode.java_spring_boot_back_end_app.models.User;
+import com.launchcode.java_spring_boot_back_end_app.repositories.UserRepository;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+public class UserController {
+}
