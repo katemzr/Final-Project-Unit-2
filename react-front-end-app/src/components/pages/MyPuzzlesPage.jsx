1b255001d04be478de5229301acf6979b2b2a9b7
katemzr <katemzr@gmail.com>
2026-09-14 12:05:20 -0500

    removed old code -hardcoded puzzles- from MyPuzzlesPage.jsx

--- react-front-end-app/src/components/pages/MyPuzzlesPage.jsx
+++ react-front-end-app/src/components/pages/MyPuzzlesPage.jsx
@@ -3,48 +3,6 @@ import PuzzleCard from "../PuzzleCard";
 import { useEffect, useState } from "react";
 import apiClient from "../../config/api-client";
 
-/*
-const initialPuzzles = [  //puzzles to appear on My Puzzles page to simulate a user's saved puzzle cards.
-    {
-        id: "1", 
-        title: "My Cup Runneth Over",
-        link: '/images/MyCupRunnethOver.jpg',
-        alt: "image of My Cup Runneth Over puzzle"
-
-    },{
-        id: "2",
-        title: "Worship of Trees",
-        link: '/images/WorshipOfTrees.jpg',
-        alt: "image of Worship of Trees puzzle"
-
-    },{
-        id: "3",
-        title: "The Exploded Beetle",
-        link: '/images/TheExplodedBeetle.jpg',
-        alt: "image of the Exploded Beetle puzzle"
-
-    },{
-        id: "4",
-        title: "Bottle Caps",
-        link: '/images/BottleCaps.jpg',
-        alt: "image of Bottle Caps puzzle"
-
-    },{
-        id: "5",
-        title: "Turbo 3000",
-        link: '/images/Turbo3000.jpg',
-        alt: "image of Turbo 3000 puzzle"
-
-    },{
-        id: "6",
-        title: "Tree of Life Tapestry",
-        link: '/images/TreeOfLifeTapestry.jpg',
-        alt: "image of Tree of Life Tapestry puzzle"
-    }
-
-
-];
-*/
 
 const MyPuzzlesPage = () => {
   
@@ -79,29 +37,7 @@ const MyPuzzlesPage = () => {
         }
     };
 
-/*
-    const puzzleItems = puzzles.map(puzzle => 
-        <PuzzleCard 
-            key={puzzle.id} 
-            link={puzzle.link} 
-            alt={puzzle.alt} 
-            image={puzzle.image}
-            title={puzzle.title} 
-            puzzlebrand={puzzle.puzzlebrand} 
-            puzzleartist={puzzle.puzzleartist} 
-            piececount={puzzle.piececount} 
-            height={puzzle.height}
-            width={puzzle.width}
-            location={puzzle.location} 
-            purchasedate={puzzle.purchasedate} 
-            retailer={puzzle.retailer} 
-            startdate={puzzle.startdate} 
-            progresspercent={puzzle.progresspercent}
-            completiondate={puzzle.completiondate}
-            completiontime={puzzle.completiontime}
-            onloan={puzzle.onloan}
-            notes={puzzle.notes} /> );
-*/
+
 
 const puzzleItems = puzzles.map(puzzle => 
         <PuzzleCard 
