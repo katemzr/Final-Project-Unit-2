2fc53688a0ac3cf21bc44ad6538c39d558df7ac9
katemzr <katemzr@gmail.com>
2026-09-16 10:56:57 -0500

    added old hardcoded puzzles that had previously been used in the My Puzzles page to the new PuzzleSuggestionsPage.jsx

---
+++ react-front-end-app/src/components/pages/PuzzleSuggestionsPage.jsx
@@ -0,0 +1,41 @@
+const suggestedPuzzles = [  //hardcoded puzzles to appear on the Puzzle Suggestions page
+    {
+        id: "1", 
+        title: "My Cup Runneth Over",
+        imageURL: '/images/MyCupRunnethOver.jpg',
+        alt: "image of My Cup Runneth Over puzzle"
+
+    },{
+        id: "2",
+        title: "Worship of Trees",
+        imageURL: '/images/WorshipOfTrees.jpg',
+        alt: "image of Worship of Trees puzzle"
+
+    },{
+        id: "3",
+        title: "The Exploded Beetle",
+        imageURL: '/images/TheExplodedBeetle.jpg',
+        alt: "image of the Exploded Beetle puzzle"
+
+    },{
+        id: "4",
+        title: "Bottle Caps",
+        imageURL: '/images/BottleCaps.jpg',
+        alt: "image of Bottle Caps puzzle"
+
+    },{
+        id: "5",
+        title: "Turbo 3000",
+        imageURL: '/images/Turbo3000.jpg',
+        alt: "image of Turbo 3000 puzzle"
+
+    },{
+        id: "6",
+        title: "Tree of Life Tapestry",
+        imageURL: '/images/TreeOfLifeTapestry.jpg',
+        alt: "image of Tree of Life Tapestry puzzle"
+    }
+
+
+];
+
