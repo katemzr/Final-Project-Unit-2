4516ea769ab01e04de893e146b3816bd53da1f89
katemzr <katemzr@gmail.com>
2026-09-21 15:01:18 -0500

    deleted old DeletePuzzle.jsx code.

--- react-front-end-app/src/components/DeletePuzzle.jsx
+++ react-front-end-app/src/components/DeletePuzzle.jsx
@@ -1,16 +1,3 @@
-/*
-const DeletePuzzle = ({ onDelete }) => {
-   
-    return (
-        <button className="buttons" onClick={onDelete}>Delete</button>
-        
-    );
-}
-
-export default DeletePuzzle;
-*/
-
-
 import { useState } from "react";
 
 const DeletePuzzle = ({ onDelete }) => {
